0a7b7f18712e6a7068ffae524228e55977d7f712
andbudha <andreibartov@gmail.com>
2024-04-09 09:03:42 +0200

    routes for serving html files handled

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'index');
-  res.send(filePath);
+app.get('/', function (req, res) {
+  const filePath = path.join(__dirname, 'views', 'index.html');
+  res.sendFile(filePath);
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', function (req, res) {
   const filePath = path.join(__dirname, 'views', 'about.html');
-  res.send(filePath);
+  res.sendFile(filePath);
+});
+app.get('/confirmation', function (req, res) {
+  const filePath = path.join(__dirname, 'views', 'confirmation.html');
+  res.sendFile(filePath);
+});
+
+app.get('/contact', function (req, res) {
+  const filePath = path.join(__dirname, 'views', 'contact.html');
+  res.sendFile(filePath);
+});
+app.get('/recipes', function (req, res) {
+  const filePath = path.join(__dirname, 'views', 'recipes.html');
+  res.sendFile(filePath);
+});
+app.get('/share', function (req, res) {
+  const filePath = path.join(__dirname, 'views', 'share.html');
+  res.sendFile(filePath);
 });
 
 app.listen(3000);
