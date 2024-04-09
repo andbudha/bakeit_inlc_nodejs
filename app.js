8777092133f71d4801fd2f0a719a39677f9ce001
andbudha <andreibartov@gmail.com>
2024-04-09 10:25:53 +0200

    collecting and storing share-form data func-ty added

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const app = express();
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   const filePath = path.join(__dirname, 'views', 'index.html');
@@ -28,6 +30,15 @@ app.get('/recipes', function (req, res) {
 app.get('/share', function (req, res) {
   const filePath = path.join(__dirname, 'views', 'share.html');
   res.sendFile(filePath);
+});
+app.post('/share', (req, res) => {
+  const recipe = req.body;
+  const filePath = path.join(__dirname, 'data', 'recipes.json');
+  const fileData = fs.readFileSync(filePath);
+  const recipes = JSON.parse(fileData);
+  recipes.push(recipe);
+  fs.writeFileSync(filePath, JSON.stringify(recipes));
+  res.redirect('/confirmation');
 });
 
 app.listen(3000);
